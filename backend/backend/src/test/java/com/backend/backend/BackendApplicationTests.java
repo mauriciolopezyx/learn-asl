26413f2edede95169b6b2c82dbd50bb9576af87a
orangetbh <mauriciolopezyx@gmail.com>
2025-08-28 22:49:12 -0700

    Adding first part of integration test to ensure compatability with Maven action (fixing default contextLoads)

--- backend/backend/src/test/java/com/backend/backend/BackendApplicationTests.java
+++ backend/backend/src/test/java/com/backend/backend/BackendApplicationTests.java
@@ -1,10 +1,38 @@
 package com.backend.backend;
 
+import com.backend.backend.app.SecurityConfigTest;
+import com.backend.backend.email.EmailConfigTest;
+import com.redis.testcontainers.RedisContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+@Import({EmailConfigTest.class, SecurityConfigTest.class})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BackendApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
+
+	@Container
+	static RedisContainer redis = new RedisContainer(DockerImageName.parse("redis:7.2.5"));
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.redis.host", redis::getHost);
+		registry.add("spring.data.redis.port", redis::getFirstMappedPort);
+	}
 
 	@Test
 	void contextLoads() {
